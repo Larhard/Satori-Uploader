1aad1cb302f9d1b7870278411bbe64a74f44ef13
Larhard <larhard@gmail.com>
2014-04-02 20:09:46 +0200

    it works

--- uploader.py
+++ uploader.py
@@ -3,36 +3,112 @@
 from satori_api import API, LoginFailedException
 import argparse
 import re
+import time
 
 
-def main(args):
+def main(parser):
+    args = parser.parse_args()
     try:
         api = API(verbose=True)
     except LoginFailedException:
         print "Login Failed"
         return
 
+    def get_contest(contest):
+        if re.match('^@\d+$', contest):
+            return api.get_contests()[int(contest[1:])]['id']
+        if re.match('^\d+$', contest):
+            return int(contest)
+        return None
+
+    def get_problem(contest, problem):
+        if re.match('^@\d+$', problem):
+            return api.get_submittable_problems(contest)[int(problem[1:])]['id']
+        if re.match('^\d+$', problem):
+            return int(problem)
+        return None
+
     if args.list:
         for idx, entry in enumerate(api.get_contests()):
             print "{:3d} : {} ({})".format(idx, entry['name'], entry['id'])
+
     elif args.results:
-        contest = None
-        if re.match('^@\d+$', args.results):
-            contest = int(args.results[1:])
-        elif re.match('^\d+$', args.results):
-            contest = api.get_contests()[int(args.results)]['id']
+        if not args.contest:
+            print "Select contest"
+            return
+
+        contest = get_contest(args.contest)
         if contest is None:
             print "Bad contest id"
             return
 
-        for entry in api.get_results(contest):
+        data = api.get_results(contest)
+        for entry in data['results']:
             print "{}\t[ {} ]   \t({})".format(entry['problem'], entry['status'], entry['date'])
+
+    elif args.available_problems:
+        if not args.contest:
+            print "Select contest"
+            return
+
+        contest = get_contest(args.contest)
+        if contest is None:
+            print "Bad contest id"
+            return
+
+        data = api.get_submittable_problems(contest)
+        for idx, entry in enumerate(data):
+            print "{:3d} : {} : {}   [{}]".format(idx, entry['code'], entry['name'], entry['id'])
+
+    elif args.submit:
+        if not args.contest:
+            print "Select contest"
+            return
+        if not args.problem:
+            print "Select problem"
+            return
+        if not args.file:
+            print "Select file"
+            return
+
+        contest = get_contest(args.contest)
+        problem = get_problem(contest, args.problem)
+        file_path = args.file
+
+        api.submit(contest, problem, file_path)
+
+    if args.wait:
+        try:
+            while True:
+                if not args.contest:
+                    print "Select contest"
+                    return
+
+                contest = get_contest(args.contest)
+                if contest is None:
+                    print "Bad contest id"
+                    return
+
+                data = api.get_results(contest)
+                for entry in data['results']:
+                    print "{}\t[ {} ]   \t({})".format(entry['problem'], entry['status'], entry['date'])
+
+                print "---\n"
+                time.sleep(10)
+        except KeyboardInterrupt:
+            pass
 
     # TODO : the rest
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--list', '-l', action='store_true', help="list available contests")
-    parser.add_argument('--results', '-r', metavar='id', help="show last results of contest with given @id / idx")
-    main(parser.parse_args())
+    parser.add_argument('-l', '--list', action='store_true', help="list available contests")
+    parser.add_argument('-r', '--results', action='store_true', help="show results")
+    parser.add_argument('-c', '--contest', metavar='id', help="select contest with given @id / idx")
+    parser.add_argument('-a', '--available-problems', action='store_true', help="list available problems")
+    parser.add_argument('-f', '--file', help="select file")
+    parser.add_argument('-p', '--problem', help="select problem")
+    parser.add_argument('-s', '--submit', action='store_true', help="submit solution")
+    parser.add_argument('-w', '--wait', action='store_true', help="wait for results")
+    main(parser)
