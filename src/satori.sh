6afb66d931c6ed25522eabf28254e836a7813791
Larhard <larhard@gmail.com>
2014-10-07 16:38:26 +0200

    auto local include in C/C++

--- src/satori.sh
+++ src/satori.sh
@@ -4,8 +4,6 @@ PYTHON=python2
 BASE_DIR=$(dirname $(readlink -e $0))
 CONFIG=$HOME/.satori-uploader/
 
-pushd $BASE_DIR > /dev/null
 
-$PYTHON uploader.py --config $CONFIG "$@"
+$PYTHON "${BASE_DIR}/uploader.py" --config $CONFIG "$@"
 
-popd > /dev/null
--- src/satori_api.py
+++ src/satori_api.py
@@ -160,6 +160,16 @@ class API:
                               "\\g<commentary>//\\g<package>",
                               data,
                               flags=re.MULTILINE)
+            if re.search('\.(c|cpp)$', file_name):
+                # include local
+                includes = re.finditer('^\s*#include\s*"([^"]*)"\s*$', data, re.MULTILINE)
+                for i in includes:
+                    print i.group(1)
+                    # open(i.group(1)).read()
+                    data = open(i.group(1)).read().join(data.split(i.group(0)))
+                    # data = open(i.group(1)).join()
+                    # data = re.sub(i.group(0), "---", data, re.MULTILINE)
+
 
         mp.add_file('codefile', file_name, data)
         content = mp.get_content()
